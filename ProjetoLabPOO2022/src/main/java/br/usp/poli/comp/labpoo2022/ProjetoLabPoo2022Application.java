a150c5fda8bd3527be19bb64ad302db7018ba77b
Igor Pontes Tresolavy <tresolavy@usp.br>
2022-05-11 07:07:27 -0300

    initial commit

---
+++ ProjetoLabPOO2022/src/main/java/br/usp/poli/comp/labpoo2022/ProjetoLabPoo2022Application.java
@@ -0,0 +1,13 @@
+package br.usp.poli.comp.labpoo2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLabPoo2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLabPoo2022Application.class, args);
+	}
+
+}
