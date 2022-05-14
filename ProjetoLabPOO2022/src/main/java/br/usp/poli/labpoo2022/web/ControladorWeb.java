5b2cb0bed7f0d83f57dcb0dfb102513d893386a6
Gabriel Chaves Lopes Silva <gabriel.clsilva@usp.br>
2022-05-14 21:01:35 +0000

    Atualização do home.html e mudança na classe WebController.

--- ProjetoLabPOO2022/src/main/java/br/usp/poli/labpoo2022/web/ControladorWeb.java
+++ ProjetoLabPOO2022/src/main/java/br/usp/poli/labpoo2022/web/ControladorWeb.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class WebController {
+public class ControladorWeb {
 	@GetMapping(value="/")
 	public String home() {
 		return "home";
